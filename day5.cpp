05234fb69cbbd541170a9ad57aefc71fdfaa73f1
Priyanshi Garg <116329214+PriyanshiG7@users.noreply.github.com>
2023-09-22 12:03:37 +0530

    Add files via upload

---
+++ day5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if (x<=15){
+	        cout<<"yes";
+	    }else{
+	        cout<<"no";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
