3d733641490cae55110b7bac41b3eefe8f3d8ff2
Priyanshi Garg <116329214+PriyanshiG7@users.noreply.github.com>
2023-09-25 23:34:32 +0530

    Add files via upload

---
+++ day8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int t, X, Y;
+    cin >>t;
+    while(t--){
+    int x,y;cin>>x>>y;
+        cout<<x/(2*y)<<endl;
+}
+    return 0;
+}
