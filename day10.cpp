1dc4ba1ec8d3d62c36a9cdc126a5958362b5fc54
Priyanshi Garg <116329214+PriyanshiG7@users.noreply.github.com>
2023-09-28 13:17:26 +0530

    Add files via upload

---
+++ day10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,d;
+	    cin>>x>>y;
+	    d=x/y;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
